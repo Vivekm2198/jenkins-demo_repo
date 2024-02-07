f198eb0889ce831813306e1a365b27e0d40955d0
Vivekm2198 <158180554+Vivekm2198@users.noreply.github.com>
2024-02-07 19:23:26 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,7 @@
 if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   echo "Build was successful, running additional steps..."
   # Add additional commands here
-  touch cron2
+  touch cron3
 else
   echo "Build failed, skipping additional steps."
 fi
