02050f8b31e2bb904ef4f43a3ba3b089c08aced0
Vivekm2198 <158180554+Vivekm2198@users.noreply.github.com>
2024-02-08 20:25:23 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,1 @@
-if [ "$BUILD_STATUS" = "SUCCESS" ]; then
-  echo "Build was successful, running additional steps..."
-  echo " "
-  
-else
-  echo "Build failed, skipping additional steps."
-fi
+we are demostrating jenkins jon for git webhooks
